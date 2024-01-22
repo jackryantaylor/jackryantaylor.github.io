fa6751160fa66b840ad20b1e69d164fc97ce5ffa
JackTaylor.exe <70320131+jackryantaylor@users.noreply.github.com>
2024-01-22 21:48:10 +0000

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,41 @@
+body {
+    background-color: #110e24;
+    max-width: 750px;
+    margin: auto;
+    padding-top: 100px;
+    min-height: 200vh
+}
+
+h1 {
+    color: whitesmoke;
+    letter-spacing: 3px;
+    font-family: spartan;
+    text-align: left;
+    font-size: 50px;
+}
+
+h3 {
+    margin-top: 30px;
+    color: #00ffaa;
+    letter-spacing: 1.5px;
+    font-family: lato;
+    font-size: 20px;
+}
+
+p {
+    margin-top: 60px;
+    color: white;
+    letter-spacing: 1px;
+    font-family: lato;
+    font-size: 15px;
+}
+
+@font-face {
+    font-family: spartan;
+    src: url(LeagueSpartan-Bold.otf);
+    }
+
+@font-face {
+    font-family: lato;
+    src: url(Lato-Regular.ttf);
+}
