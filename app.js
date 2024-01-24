bc8ef8c69449bb057d666b0e96d6f0cecf356467
JackTaylor.exe <70320131+jackryantaylor@users.noreply.github.com>
2024-01-24 22:47:47 +0000

    Added dotted background

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show")
-        } else {
-            entry.target.classList.remove("show")
-        }
-    })
-})
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add("show")
+//         } else {
+//             entry.target.classList.remove("show")
+//         }
+//     })
+// })
 
-const animated = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+// const animated = document.querySelectorAll(".hidden");
+// hiddenElements.forEach((el) => observer.observe(el));
